86694ae11f377bde4340a730e7d63852b86c2338
Samira-Git <siam27most@gmail.com>
2020-09-26 00:03:08 +0200

    update v

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+
+  Images = [
+    "assets/img/ananas.jpg",
+    "assets/img/aubergine.jpg",
+    "assets/img/bananes.jpg",
+    "assets/img/carottes.jpg",
+    "assets/img/cerises.jpg",
+    "assets/img/courgettes.jpg"
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    autoplay: true,
+    autoplayTimeout: 1520,
+    smartSpeed: 1500,
+    animateIn: 'linear',
+    animateOut: 'linear',
+    dots: true,
+    stagePadding: 10,
+    lazyLoad: true,
+    margin: 20,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1,
+        margin: 20,
+        nav: true,
+        navSpeed: 100
+      },
+      400: {
+        items: 1, 
+        margin: 20,
+        nav: true,
+        navSpeed: 100
+      },
+      740: {
+        items: 2,
+        margin: 20
+      },
+      940: {
+        items: 3,
+        margin: 20
+      }
+    },
+    nav: true
+
+  }
+
+}
