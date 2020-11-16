489123c7e7c356a1712ac88cbadd721fe480dd6e
Samira-Git <siam27most@gmail.com>
2020-11-16 22:30:42 +0100

    ajout du panier sous forme de tableau, méthode de calcul du total et de quantité, + css

--- ShoppingCard/src/app/app-routing.module.ts
+++ ShoppingCard/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AccueilComponent } from './accueil/accueil.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { DetailProduitComponent } from './detail-produit/detail-produit.component';
 import { ListeProductsComponent } from './liste-products/liste-products.component';
+import { PanierComponent } from './panier/panier.component';
 import { SearchComponent } from './search/search.component';
 
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: '', redirectTo : '/accueil', pathMatch : 'full'},
   {path: 'accueil', component : AccueilComponent},
   {path : 'catalog', component : CatalogComponent},
+  {path : 'panier', component : PanierComponent},
   {path : 'search', component : SearchComponent},
   {path : 'liste/:type', component : ListeProductsComponent},
   {path : 'product/:id', component : DetailProduitComponent}
--- ShoppingCard/src/app/panier/panier.component.ts
+++ ShoppingCard/src/app/panier/panier.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-panier',
@@ -6,10 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panier.component.scss']
 })
 export class PanierComponent implements OnInit {
-
-  constructor() { }
+  productsCard;
+  total;
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.productsCard = this.data.productPanier;
+    this.calculerTotal();
+    this.data.obsTotal.subscribe(value => {
+      if (value) {
+        this.calculerTotal();
+      }
+    })
+  }
+  //méthode pour calculer le prix total du panier
+  calculerTotal = () => {
+    this.total = 0;
+    this.productsCard.forEach(element => {
+      let calcul = parseFloat(this.data.produits.find(p => p.id == element.id).prix) * element.qty + parseFloat(this.total);
+      this.total = calcul.toFixed(2);
+    });
   }
 
+  //retirer le produit du panier
+  delete = (id) => {
+    let prod = this.productsCard.find(elt => elt.id == id);
+    let index = this.productsCard.indexOf(prod);
+    this.productsCard.splice(index, 1);
+    this.data.obsTotal.next(true);
+  }
+
+  //diminuer la quantité du produit
+  updateMoins = (id) => {
+    let prod = this.productsCard.find(elt => elt.id == id);
+    if (prod.qty > 1) {
+      prod.qty--;
+      this.data.obsTotal.next(true);
+    }
+    else {
+      this.delete(id);
+    }
+  }
+
+  //augmenter la quantité
+  updatePlus = (id) => {
+    let prod = this.productsCard.find(elt => elt.id == id);
+    prod.qty++;
+    this.data.obsTotal.next(true);
+  }
 }
